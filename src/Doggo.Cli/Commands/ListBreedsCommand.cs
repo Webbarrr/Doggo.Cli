4f428364b81e544bdc072fefb4e53e0ba37ad23d
webbarr <jameswebber90@gmail.com>
2025-06-19 21:35:03 +0100

    minor formatting change

--- src/Doggo.Cli/Commands/ListBreedsCommand.cs
+++ src/Doggo.Cli/Commands/ListBreedsCommand.cs
@@ -2,30 +2,29 @@
 using Spectre.Console;
 using Spectre.Console.Cli;
 
-namespace Doggo.Cli.Commands
+namespace Doggo.Cli.Commands;
+
+public class ListBreedsCommand : AsyncCommand
 {
-    public class ListBreedsCommand : AsyncCommand
+    private readonly IDoggoClient _doggoClient;
+
+    public ListBreedsCommand(IDoggoClient doggoClient)
     {
-        private readonly IDoggoClient _doggoClient;
+        _doggoClient = doggoClient;
+    }
 
-        public ListBreedsCommand(IDoggoClient doggoClient)
-        {
-            _doggoClient = doggoClient;
-        }
+    public override async Task<int> ExecuteAsync(CommandContext context)
+    {
+        var breeds = await _doggoClient.ListBreedsAsync();
 
-        public override async Task<int> ExecuteAsync(CommandContext context)
-        {
-            var breeds = await _doggoClient.ListBreedsAsync();
+        var table = new Spectre.Console.Table();
+        table.AddColumn("Breed");
 
-            var table = new Spectre.Console.Table();
-            table.AddColumn("Breed");
+        foreach (var breed in breeds)
+            table.AddRow(breed);
 
-            foreach (var breed in breeds)
-                table.AddRow(breed);
+        AnsiConsole.Write(table);
 
-            AnsiConsole.Write(table);
-
-            return 0;
-        }
+        return 0;
     }
 }
